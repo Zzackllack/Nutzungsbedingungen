efb44da98ff6febe21d01f873e7eb640dddcd0e7
Zacklack <contact@zacklack.de>
2025-02-10 19:33:12 +0100

    Update Vite base path from '/impressum/' to '/nutzungsbedingungen/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/impressum/",
+  base: "/nutzungsbedingungen/",
   plugins: [react()],
   resolve: {
     alias: {
